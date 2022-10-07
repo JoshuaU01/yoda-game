77996baf728d4d96648da13e6117ae2f6b6aa493
Joshua Uhl <jouhit00@hs-esslingen.de>
2022-10-07 15:22:39 +0200

    restructure

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     pygame.init()
     clock = pygame.time.Clock()
 
-    pygame.display.set_caption("Ahh yes game")
+    pygame.display.set_caption("Game")
     screen_size_x = 1200
     screen_size_y = 595
     screen = pygame.display.set_mode([screen_size_x, screen_size_y])
