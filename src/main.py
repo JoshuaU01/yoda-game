57a136f3fb322d0cd99b254f4ef43527cb3c691b
Joshua Uhl <joshua.uhl2001@gmail.com>
2022-10-08 22:10:22 +0200

    added background graphic

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pygame
 def check_exit():
     for event in pygame.event.get():
@@ -10,13 +11,21 @@ if __name__ == '__main__':
 
     pygame.init()
     clock = pygame.time.Clock()
+    os.chdir("..")
 
     pygame.display.set_caption("Game")
-    screen_size_x = 1200
-    screen_size_y = 595
+    screen_size_x = 1800
+    screen_size_y = 950
     screen = pygame.display.set_mode([screen_size_x, screen_size_y])
+
+    background = pygame.image.load("media/images/background/map_gras.png")
+    background = pygame.transform.scale(background, (1800, 950))
 
     while True:
         check_exit()
+        screen.blit(background, (0, 0))
+
+
+
         pygame.display.update()
         clock.tick(60)
