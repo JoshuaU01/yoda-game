917b27bb6413ccfc3718dd2ef4eef3872c409fbc
Joshua Uhl <jouhit00@hs-esslingen.de>
2025-01-05 20:02:14 +0100

    Moved runner specific states into runner.py

--- src/assets/characters/enemies/runner.py
+++ src/assets/characters/enemies/runner.py
@@ -6,7 +6,7 @@ from src.asset import Asset
 from src.assets.characters.enemy import Enemy
 from src.assets.characters.player import Player
 from src.environment.world import World, Directions
-from src.utils.state import StateManager, WalkState, RunState, PrepareAttackState, StompState
+from src.utils.state import State, StateManager
 
 
 class Runner(Enemy):
@@ -121,3 +121,95 @@ class Runner(Enemy):
         """
         if not isinstance(self.state, StompState) and self.stomp_cooldown > 0:
             self.stomp_cooldown -= 1
+
+
+class WalkState(State):
+
+    @property
+    def runner(self):
+        return self.state_manager.owner
+
+    def update(self) -> None:
+        if self.runner.target:
+            if self.runner.is_near(self.runner.target, self.runner.attack_range):
+                self.state_manager.change_state("prepare_attack")  # Transition T5
+            else:
+                self.state_manager.change_state("run")  # Transition T1
+
+    def execute(self) -> None:
+        self.runner.velocity.x = self.runner.direction * self.runner.speed
+        old_x = self.runner.rect.x  # Save the current horizontal position
+        self.runner.rect.x += self.runner.velocity.x
+        if self.runner.collision:  # Pre-check, if the owner would collide with something
+            self.runner.turn_around()
+            self.runner.velocity.x *= -1  # Walk into the other direction
+        self.runner.rect.x = old_x  # Always reset the horizontal position, as the position update will be done later
+
+
+class RunState(State):
+
+    @property
+    def runner(self):
+        return self.state_manager.owner
+
+    def update(self):
+        if not self.runner.target:
+            self.state_manager.change_state("walk")  # Transition T2
+        elif self.runner.is_near(self.runner.target, self.runner.attack_range):
+            self.state_manager.change_state("prepare_attack")  # Transition T8
+
+    def execute(self) -> None:
+        self.runner.face_target(self.runner.target)
+        self.runner.velocity.x = self.runner.direction * self.runner.speed * 3
+
+
+class PrepareAttackState(State):
+
+    @property
+    def runner(self):
+        return self.state_manager.owner
+
+    def update(self):
+        if not self.runner.target:
+            self.state_manager.change_state("walk")  # Transition T4
+        elif not self.runner.is_near(self.runner.target, self.runner.attack_range):
+            self.state_manager.change_state("run")  # Transition T3
+        elif self.runner.is_facing(self.runner.target) and self.runner.on_ground and self.runner.stomp_cooldown <= 0:
+            self.state_manager.change_state("stomp")  # Transition T6
+
+    def execute(self) -> None:
+        self.runner.face_target(self.runner.target)
+        self.runner.velocity.x = 0
+
+
+class StompState(State):
+
+    @property
+    def runner(self):
+        return self.state_manager.owner
+
+    def update(self) -> None:
+        if self.runner.on_ground and self.runner.velocity.y >= 0:  # Stomp action must be completed to exit this state
+            if not self.runner.target:
+                self.state_manager.change_state("walk")  # Transition T9
+            elif not self.runner.is_near(self.runner.target, self.runner.attack_range):
+                    self.state_manager.change_state("run")  # Transition 10
+            else:
+                self.state_manager.change_state("prepare_attack")  # Transition T7
+
+    def enter(self):
+        """
+        Start stomping.
+        """
+        self.runner.velocity.y = -12
+        self.runner.stomp_cooldown = 90
+
+    def execute(self) -> None:
+        pass
+
+    def exit(self):
+        if self.runner.target and self.runner.is_near(self.runner.target, (self.runner.detect_range[0], 40)):
+            if self.runner.target.can_take_damage and hasattr(self.runner.target, "take_damage"):
+                self.runner.target.take_damage(1)  # Only vulnerable assets take damage
+            print(f"{self.runner.target} got hit!")
+        # TODO shake the camera (observer)
